26c77c72425aa22a1713c6308be9bba58a1e9652
Davide Melfi <darklight3it@gmail.com>
2023-02-04 10:49:00 +0100

    chore: fixed character rotation

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -3,20 +3,24 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    private const float ForwardDirectionDegrees = 0f;
+    private const float ForwardDirectionDegrees = 180;
 
+    //public PlayerConfig playerConfig;
     [SerializeField]
-    private float speed = 1f;
+    private float speed = 2f;
     [SerializeField]
     private float rotationSpeed = 25f;
     [SerializeField]
-    [Range(45f, 75f)]
+    [Range(45f, 90f)]
     private float maxRotationDegrees = 60f;
 
+    //private PlayerInput playerInput;
     private PlayerInputActions playerInputActions;
 
+    // Start is called before the first frame update
     private void Awake()
     {
+        //playerInput = GetComponent<PlayerInput>();
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
     }
@@ -42,28 +46,15 @@ public class PlayerManager : MonoBehaviour
             transform.rotation = Quaternion.Euler(0, 0, targetAngle);
         }
 
-        //transform.position -= transform.up * speed * Time.deltaTime;
-        Vector2 newPosition = transform.position - speed * Time.deltaTime * transform.up;
-        transform.position = new Vector2(newPosition.x, transform.position.y);
+        transform.position -= transform.up * speed * Time.deltaTime;
     }
 
     private float GetTargetAngle(float moveDirection)
     {
-        float targetAngle = transform.rotation.eulerAngles.z;
-        if (targetAngle > 180f)
-        {
-            targetAngle -= 360f;
-        }
+        float currentAngle = transform.rotation.eulerAngles.z;
 
-        if (moveDirection < 0f && targetAngle > ForwardDirectionDegrees - maxRotationDegrees)
-        {
-            targetAngle -= rotationSpeed * Time.deltaTime;
-        }
-        else if (moveDirection > 0f && targetAngle < ForwardDirectionDegrees + maxRotationDegrees)
-        {
-            targetAngle += rotationSpeed * Time.deltaTime;
-        }
-
-        return targetAngle;
+        float newAngle = currentAngle + Math.Sign(moveDirection) * rotationSpeed * Time.deltaTime;
+        
+        return maxRotationDegrees < Math.Abs(ForwardDirectionDegrees - newAngle) ? newAngle : currentAngle;
     }
 }
